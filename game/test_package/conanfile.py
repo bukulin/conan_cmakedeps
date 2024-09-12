fdd9eeb0558e3ff05e999b758d0dee679bf5e074
nbukuli <norbert.bukuli@mediso.com>
2024-09-12 12:50:43 +0000

    game: test_package: throw generated files into a subdirectory

--- game/test_package/conanfile.py
+++ game/test_package/conanfile.py
@@ -1,6 +1,7 @@
 import os
 from conan import ConanFile
 from conan.tools.build import can_run
+from conan.tools.layout import basic_layout
 
 
 class gameTestConan(ConanFile):
@@ -12,3 +13,6 @@ class gameTestConan(ConanFile):
     def test(self):
         if can_run(self):
             self.run("game", env="conanrun")
+
+    def layout(self):
+        basic_layout(self)
